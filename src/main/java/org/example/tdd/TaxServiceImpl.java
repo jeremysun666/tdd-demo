692ec99772a997e28ad0006e89bfbc36cd20ac4d
Jeremy Sun <yunxuan_sun@apple.com>
2020-07-22 14:22:02 -0700

    b105

--- src/main/java/org/example/tdd/TaxServiceImpl.java
+++ src/main/java/org/example/tdd/TaxServiceImpl.java
@@ -9,8 +9,10 @@ public class TaxServiceImpl implements TaxService {
 			return Math.round(income * 0.1f);
  		} else if (income <= 100000) {
 			return Math.round(5000 + (income - 50000) * 0.2f);
-		} else {
+		} else if (income <= 200000){
 			return Math.round(15000 + (income - 100000) * 0.25f);
+		} else {
+			return Math.round(40000 + (income - 200000) * 0.5f);
 		}
 	}
 }
