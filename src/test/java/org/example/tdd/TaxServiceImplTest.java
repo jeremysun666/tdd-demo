39f155c95f31d03e496456d8b85c13aca57b8886
Jeremy Sun <yunxuan_sun@apple.com>
2020-07-23 09:21:44 -0700

    b101

---
+++ src/test/java/org/example/tdd/TaxServiceImplTest.java
@@ -0,0 +1,37 @@
+package org.example.tdd;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TaxServiceImplTest {
+
+	private TaxService taxService = new TaxServiceImpl();
+
+	@Test
+	public void test_0() {
+		assertEquals(0, taxService.getIncomeTax(0));
+	}
+
+	@Test
+	public void test_50000() {
+		assertEquals(5000, taxService.getIncomeTax(50000));
+	}
+
+	@Test
+	public void test_100000() {
+		//50 * 0.1 + 50 * 20%
+		assertEquals(15000, taxService.getIncomeTax(100000));
+	}
+
+	@Test
+	public void test_invalid() {
+		assertEquals(0, taxService.getIncomeTax(-1));
+	}
+
+	@Test
+	public void test_200000() {
+		//15000 + (100000 * 0.25
+		assertEquals(40000, taxService.getIncomeTax(200000));
+	}
+}
